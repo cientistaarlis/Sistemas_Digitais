54337a89c49c54731d0b111aa1380ba8d3b6eb09
cientistaarlis <dhenearlis@gmail.com>
2026-03-02 14:57:17 -0300

    Add testbench for half adder module
    This testbench simulates the half adder behavior by applying all combinations of input values and monitoring the outputs.

---
+++ Half Adder/tb_half_adder.v
@@ -0,0 +1,28 @@
+`timescale 1ns/1ps
+
+module tb_half_adder;
+    reg a;
+    reg b;
+    wire sum;
+    wire carry;
+
+    half_adder uut (.a(a), .b(b), .sum(sum), .carry(carry));
+
+    initial 
+    begin
+        $dumpfile("half_adder.vcd");
+        $dumpvars(0,tb_half_adder);
+        $monitor("time: %3d, a:%b, b:%b, sum:%b, carry:%b ",$time,a,b,sum,carry );
+
+        a=1'b0; b=1'b0; //Primeira Linha da Tabela verdade
+        #5;
+        a=1'b0; b=1'b1; //Segunda Linha da Tabela verdade  
+        #5;
+        a=1'b1; b=1'b0; //Terceira Linha da Tabela verdade
+        #5;
+        a=1'b1; b=1'b1; //Quarta Linha da Tabela verdade
+
+        #5; $finish;
+    end
+    
+endmodule
