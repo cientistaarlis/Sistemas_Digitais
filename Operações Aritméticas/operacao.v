38f71a695eb5d1ed7376941457dbccb5244b4d5e
cientistaarlis <dhenearlis@gmail.com>
2026-03-04 11:12:25 -0300

    Update operacao.v

--- Operações Aritméticas/operacao.v
+++ Operações Aritméticas/operacao.v
@@ -1,1 +1,32 @@
+module operacao #(parameter tamanho =3)
+(
+    // declaração das portas 
+    input wire [tamanho-1:0] a,
+    input wire [tamanho-1:0] b,
+    output reg [tamanho:0] add, //gera um bit a mais
+    output reg [tamanho:0] sub,
+    output reg [tamanho-1:0] logico_OR,
+    output reg [tamanho-1:0] logico_AND
+);
 
+    always @(a or b)
+        begin
+          add = a + b;  
+
+        end
+
+    always @(a or b)
+        begin 
+            sub = a - b;
+        end
+    
+    always@(*)
+        begin
+            logico_OR = a | b;        
+        end
+
+    always @(*)
+        begin
+            logico_AND = a & b;
+        end
+endmodule;
