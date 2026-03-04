903f62400a0888d635efb635338d2440dedb75bf
cientistaarlis <dhenearlis@gmail.com>
2026-03-04 11:08:50 -0300

    Update mult.v

--- Modulo Multiplicador/Modo Processual/mult.v
+++ Modulo Multiplicador/Modo Processual/mult.v
@@ -1,1 +1,14 @@
 
+module mult2 #(parameter tamanho = 3)//definir constante, caso queira mudar o tamanho, facilita
+(	
+	//Declaração das Portas
+	input wire [tamanho-1:0] a,  //entrada do modulo
+  	input wire [tamanho-1:0] b, //entrada do modulo
+  	output reg [(2*tamanho)-1:0] x  //saida do modulo - o 2*tamanho, devido a operação de multiplicação
+);
+
+	always @(*)
+		begin
+			x = a * b ; //atribuição processual e multiplicação
+		end	
+endmodule;
