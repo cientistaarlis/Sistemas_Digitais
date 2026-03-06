e899fb11c9c5507eed543ac6781fb7aff8fb79f8
cientistaarlis <dhenearlis@gmail.com>
2026-03-06 09:31:30 -0300

    Update mux.v

--- Multiplexador Condicional/mux.v
+++ Multiplexador Condicional/mux.v
@@ -1,3 +1,19 @@
+// ==========================================
+// Módulo: mux
+//
+// Descrição:
+// Implementa uma lógica condicional que determina o valor
+// da saída com base na comparação do vetor f e no sinal
+// de controle ctrl.
+//
+// Lógica implementada:
+// - Se f < 4'b0110 → d = a & b
+// - Se f >= 4'b0110 e ctrl = 1 → d = 0
+// - Caso contrário → d = c
+//
+// Autor: Dhene Arlis
+// ==========================================
+
 module mux(
     //Declaração de Portas
     input wire a,
